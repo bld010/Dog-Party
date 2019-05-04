ad65bcc2c1b1448a4e13b7ea73fe44aa4ddc4b0c
Brianna DelValle <briannadelvalle@gmail.com>
2019-05-03 20:01:26 -0600

    Add e argument to prevent automatic refresh on button click.

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ var newDogName = document.querySelector("#dogName");
 var span = document.querySelector("#changing-text");
 var button = document.querySelector("#myButton");
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function(e){
   debugger;
   span.innerHTML = newDogName.value;
   e.preventDefault(); 
+  // e.stopPropagation();
 });
 
