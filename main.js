da3d5b654fd8002ba7d7d6159dd7ced571da3bf6
Brianna DelValle <briannadelvalle@gmail.com>
2019-05-04 12:24:27 -0600

    Empty input box after submit

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ var newDogName = document.querySelector("#dogName");
 var span = document.querySelector("#changing-text");
 var button = document.querySelector("#myButton");
 
-button.addEventListener("click", function(e){
-  debugger;
+button.addEventListener("click", changeName);
+
+
+function changeName(e){
   span.innerHTML = newDogName.value;
   e.preventDefault(); 
-});
+  newDogName.value = "";
+}
 
